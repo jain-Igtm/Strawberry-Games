a5f628c8664e325466cd4b0d8ade95bdac21e33d
jain-Igtm <masterchess465@gmail.com>
2026-06-28 12:10:35 -0400

    Add zones module

---
+++ src/zones.js
@@ -0,0 +1,8 @@
+export const ZONES = {
+  wilderness: { enabled: true },
+  fantasy: { enabled: false },
+  city: { enabled: false },
+  outbreak: { enabled: false },
+  anomaly: { enabled: false },
+  backrooms: { enabled: false }
+};
