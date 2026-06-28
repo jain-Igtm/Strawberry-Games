e490bf0793230d3a3a2bae0a1ccab6c2b09a908a
jain-Igtm <masterchess465@gmail.com>
2026-06-28 12:09:36 -0400

    Add game config module

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const GAME_CONFIG = {
+  build: "v13 modular-world-foundation",
+  worldName: "Strawberry World",
+  mobileFirst: true,
+  chunkSize: 86,
+  chunkRadius: 2,
+  targetFrameRate: 60
+};
