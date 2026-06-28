6afb271e66455e70540aa4ba9959b27a30ff846f
jain-Igtm <masterchess465@gmail.com>
2026-06-28 11:43:23 -0400

    Add asset manifest foundation

---
+++ src/assets/manifest.js
@@ -0,0 +1,28 @@
+export const ASSETS = {
+  treeModels: {
+    pineTall: null,
+    broadleafTall: null,
+    deadTree: null
+  },
+  rockModels: {
+    smallRock: null,
+    largeRock: null
+  },
+  groundTextures: {
+    grass: null,
+    dirt: null,
+    rock: null
+  },
+  treeTextures: {
+    bark: null,
+    leaves: null,
+    needles: null
+  }
+};
+
+export const SETTINGS = {
+  useExternalModelsWhenAvailable: true,
+  useProceduralFallbacks: true,
+  farForestUsesBillboards: true,
+  terrainUsesTextureBlending: true
+};
