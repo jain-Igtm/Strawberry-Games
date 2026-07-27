75f5d0a02e9746c007ba629ba954156181ca6871
jain-Igtm <masterchess465@gmail.com>
2026-07-27 02:41:47 -0400

    Bump mobile performance build

--- gravenmere/scripts/configure-android.mjs
+++ gravenmere/scripts/configure-android.mjs
@@ -80,8 +80,8 @@ background = background.replace(/#[0-9A-Fa-f]{6}/, '#080B0A')
 writeFileSync(backgroundPath, background)
 
 let buildGradle = readFileSync(buildGradlePath, 'utf8')
-buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 6')
-buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.3.1"')
+buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 7')
+buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.3.2"')
 writeFileSync(buildGradlePath, buildGradle)
 
-console.log('Configured landscape, fullscreen World Explorer Android 0.3.1 (6) project.')
+console.log('Configured landscape, fullscreen World Explorer Android 0.3.2 (7) project.')
