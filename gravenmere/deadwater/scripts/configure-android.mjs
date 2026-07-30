397af722ac66134ca9cb363792d919fce3d96fb3
jain-Igtm <masterchess465@gmail.com>
2026-07-30 19:30:28 -0400

    Bump Ashfall Android to 0.18.3

--- gravenmere/deadwater/scripts/configure-android.mjs
+++ gravenmere/deadwater/scripts/configure-android.mjs
@@ -80,8 +80,8 @@ background = background.replace(/#[0-9A-Fa-f]{6}/, '#1A0D09')
 writeFileSync(backgroundPath, background)
 
 let buildGradle = readFileSync(buildGradlePath, 'utf8')
-buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 21')
-buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.18.2"')
+buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 22')
+buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.18.3"')
 writeFileSync(buildGradlePath, buildGradle)
 
-console.log('Configured landscape, fullscreen Ashfall Android 0.18.2 (21) project.')
+console.log('Configured landscape, fullscreen Ashfall Android 0.18.3 (22) project.')
