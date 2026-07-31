3bdf233f3bfdc5cf2e56ad876b1e5045e8e3a1a4
jain-Igtm <masterchess465@gmail.com>
2026-07-30 21:32:41 -0400

    Set Ashfall Android version 0.18.7

--- gravenmere/deadwater/scripts/configure-android.mjs
+++ gravenmere/deadwater/scripts/configure-android.mjs
@@ -80,8 +80,8 @@ background = background.replace(/#[0-9A-Fa-f]{6}/, '#1A0D09')
 writeFileSync(backgroundPath, background)
 
 let buildGradle = readFileSync(buildGradlePath, 'utf8')
-buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 25')
-buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.18.6"')
+buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 26')
+buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.18.7"')
 writeFileSync(buildGradlePath, buildGradle)
 
-console.log('Configured landscape, fullscreen Ashfall Android 0.18.6 (25) project.')
+console.log('Configured landscape, fullscreen Ashfall Android 0.18.7 (26) project.')
