bbe253a84d32da2e454cbcee7a1ca39a4592edc5
jain-Igtm <masterchess465@gmail.com>
2026-07-31 00:37:20 -0400

    Set Android sprite zombie version 0.19.1

--- gravenmere/deadwater/scripts/configure-android.mjs
+++ gravenmere/deadwater/scripts/configure-android.mjs
@@ -80,8 +80,8 @@ background = background.replace(/#[0-9A-Fa-f]{6}/, '#1A0D09')
 writeFileSync(backgroundPath, background)
 
 let buildGradle = readFileSync(buildGradlePath, 'utf8')
-buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 32')
-buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.19.0"')
+buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 33')
+buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.19.1"')
 writeFileSync(buildGradlePath, buildGradle)
 
-console.log('Configured landscape, fullscreen Ashfall Android 0.19.0 (32) project.')
+console.log('Configured landscape, fullscreen Ashfall Android 0.19.1 (33) project.')
