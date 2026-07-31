c1a3d24a5952f22861e62497bd14989cbd387c64
jain-Igtm <masterchess465@gmail.com>
2026-07-30 23:05:01 -0400

    Set Android zombie overhaul version 0.19.0

--- gravenmere/deadwater/scripts/configure-android.mjs
+++ gravenmere/deadwater/scripts/configure-android.mjs
@@ -80,8 +80,8 @@ background = background.replace(/#[0-9A-Fa-f]{6}/, '#1A0D09')
 writeFileSync(backgroundPath, background)
 
 let buildGradle = readFileSync(buildGradlePath, 'utf8')
-buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 31')
-buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.18.12"')
+buildGradle = buildGradle.replace(/versionCode \d+/, 'versionCode 32')
+buildGradle = buildGradle.replace(/versionName "[^"]+"/, 'versionName "0.19.0"')
 writeFileSync(buildGradlePath, buildGradle)
 
-console.log('Configured landscape, fullscreen Ashfall Android 0.18.12 (31) project.')
+console.log('Configured landscape, fullscreen Ashfall Android 0.19.0 (32) project.')
