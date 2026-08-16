f4ef4066dcd8d756f3ff5b78a5d6f58e0d4c0899
jain-Igtm <masterchess465@gmail.com>
2026-08-16 09:41:27 -0400

    Make Arthur levitation altitude joystick-controlled

--- godot-backrooms/overlay/arthur_mobile/player.gd
+++ godot-backrooms/overlay/arthur_mobile/player.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 @export var mouse_sensitivity := 0.0022
 @export var touch_sensitivity := 0.0039
 @export var levitation_speed := 6.2
+@export var levitation_vertical_speed := 5.6
 @export var swim_speed := 4.35
 
 @onready var camera_pivot: Node3D = $CameraPivot
@@ -15,7 +16,7 @@ var mobile_move := Vector2.ZERO
 var pitch := 0.0
 var psychic_levitation := false
 var water_swimming := false
-var levitation_burst := 0.0
+var levitation_vertical_input := 0.0
 
 func _ready() -> void:
 	if not OS.has_feature("mobile"):
@@ -34,9 +35,9 @@ func _apply_look(delta_pixels: Vector2, sensitivity: float) -> void:
 
 func set_psychic_levitation(value: bool) -> void:
 	psychic_levitation = value
+	levitation_vertical_input = 0.0
 	if value:
-		levitation_burst = 0.82
-		velocity.y = maxf(velocity.y, 3.8)
+		velocity.y = 0.0
 
 func toggle_psychic_levitation() -> bool:
 	set_psychic_levitation(not psychic_levitation)
@@ -44,6 +45,9 @@ func toggle_psychic_levitation() -> bool:
 
 func is_self_levitating() -> bool:
 	return psychic_levitation
+
+func set_levitation_vertical_input(value: float) -> void:
+	levitation_vertical_input = clampf(value, -1.0, 1.0)
 
 func set_water_swimming(value: bool) -> void:
 	water_swimming = value
@@ -68,10 +72,35 @@ func _desktop_move() -> Vector2:
 		out.y -= 1.0
 	return out.limit_length(1.0)
 
+func _desktop_levitation_vertical() -> float:
+	var out := 0.0
+	if Input.is_key_pressed(KEY_SPACE):
+		out += 1.0
+	if Input.is_key_pressed(KEY_CTRL) or Input.is_key_pressed(KEY_C:
+		out -= 1.0
+	return clampf(out, -1.0, 1.0)
+
 func _physics_process(delta: float) -> void:
 	var move_input := (_desktop_move() + mobile_move).limit_length(1.0)
 
-	if psychic_levitation or water_swimming:
+	if psychic_levitation and not water_swimming:
+		var forward := -global_transform.basis.z
+		forward.y = 0.0
+		forward = forward.normalized()
+		var right := global_transform.basis.x
+		right.y = 0.0
+		right = right.normalized()
+		var desired := (right * move_input.x + forward * move_input.y)
+		velocity.x = desired.x * levitation_speed
+		velocity.z = desired.z * levitation_speed
+		var vertical := levitation_vertical_input
+		if not OS.has_feature("mobile"):
+			vertical = _desktop_levitation_vertical()
+		velocity.y = vertical * levitation_vertical_speed
+		move_and_slide()
+		return
+
+	if water_swimming:
 		var camera_forward: Vector3 = (-camera.global_transform.basis.z).normalized()
 		var camera_right: Vector3 = camera.global_transform.basis.x.normalized()
 		camera_right.y = 0.0
@@ -79,11 +108,7 @@ func _physics_process(delta: float) -> void:
 		var desired_3d: Vector3 = (camera_right * move_input.x + camera_forward * move_input.y)
 		if desired_3d.length_squared() > 0.001:
 			desired_3d = desired_3d.normalized()
-		var travel_speed: float = swim_speed if water_swimming else levitation_speed
-		velocity = desired_3d * travel_speed
-		if psychic_levitation and not water_swimming and levitation_burst > 0.0 and move_input.length() < 0.08:
-			velocity.y = 3.9 * clampf(levitation_burst / 0.3, 0.28, 1.0)
-		levitation_burst = maxf(0.0, levitation_burst - delta)
+		velocity = desired_3d * swim_speed
 		move_and_slide()
 		return
 
