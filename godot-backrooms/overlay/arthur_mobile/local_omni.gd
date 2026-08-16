982aa804d7579221d3c6ff67222a1e4c3ea75084
jain-Igtm <masterchess465@gmail.com>
2026-08-16 07:34:15 -0400

    Add distance-culling script for wall lights

---
+++ godot-backrooms/overlay/arthur_mobile/local_omni.gd
@@ -0,0 +1,14 @@
+extends OmniLight3D
+
+var player: Node3D
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player") as Node3D
+
+func _process(_delta: float) -> void:
+	if player == null:
+		player = get_tree().get_first_node_in_group("player") as Node3D
+		if player == null:
+			visible = false
+			return
+	visible = global_position.distance_to(player.global_position) < 17.0
